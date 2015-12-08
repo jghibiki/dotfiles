bfdeec42d58bf414e11b6cae101723bc73f48618
jghibiki <jghibiki.games@gmail.com>
2015-12-08 13:36:50 -0600

    make debian version of random asccii

--- .random_ascii.sh
+++ .random_ascii.sh
@@ -1,15 +1,9 @@
 
 
-at_home=`netctl status TryMeWireless | grep dead`
 
-if [ ! -n at_home ]; then
-    img=`ls .sexy_ascii_imgs | sort -R | head -n 1`;
-    cat ".sexy_ascii_imgs/$img";
+if  [ $((RANDOM % 2)) -eq 0 ]; then
+    cowfortune;
 else
-    if  [ $((RANDOM % 2)) -eq 0 ]; then
-        cowfortune;
-    else
-        img=`ls .ascii_imgs | sort -R | head -n 1`;
-        cat ".ascii_imgs/$img";
-    fi
+    img=`ls .ascii_imgs | sort -R | head -n 1`;
+    cat ".ascii_imgs/$img";
 fi
