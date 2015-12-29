51b0f1571c4a8cf1c62d89a88f7407c3c16a707b
jghibiki <jghibiki.games@gmail.com>
2015-12-29 12:57:23 -0600

    adds mods to deal with multiple monitors

--- .screenlayout/swapped.sh
+++ .screenlayout/swapped.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-xrandr --output DisplayPort-0 --mode 1920x1080 --pos 0x0 --rotate normal --primary  --output eDP-0 --mode 1366x768 --pos 1920x0 --rotate normal
+xrandr --setprovideroutputsource 1 0
+xrandr --output VIRTUAL1 --off --output DP1 --off --output HDMI1 --off --output LVDS1 --mode 1600x900 --pos 1920x0 --rotate normal --output DVI-I-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VGA1 --off
